76313abe96ddbf93bba499ccad40ed8fa1c92223
seribear <seribear@gmail.com>
2021-09-29 10:09:13 +0900

    함수명 변경

--- src/main/java/com/kt/totaleyes/common/XssFilter.java
+++ src/main/java/com/kt/totaleyes/common/XssFilter.java
@@ -1,15 +1,13 @@
 package com.kt.totaleyes.common;
 
 public class XssFilter {
-	public static String XssReplace(String param) {
-		param = param.replaceAll("<", "&lt;");
-		param = param.replaceAll(">", "&gt;");
-		return param;
+	public static String escape(String param) {
+		return param.replaceAll("<", "&lt;")
+					.replaceAll(">", "&gt;");
 	}
 	
-	public static String ReXssReplace(String param) {
-		param = param.replaceAll("&lt;", "<");
-		param = param.replaceAll("&gt;", ">");
-		return param;
+	public static String unescape(String param) {
+		return param.replaceAll("&lt;", "<")
+				.replaceAll("&gt;", ">");
 	}
 }
